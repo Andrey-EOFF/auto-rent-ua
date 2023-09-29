714e24f53be57a8ffc70bd8a246b37adb529e5ba
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-09-29 16:56:28 +0300

    header-fix

--- src/components/SideBar/SideBar.styled.jsx
+++ src/components/SideBar/SideBar.styled.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import { colors } from "../../vars"; 
+import { colors } from "../../vars";
 
 export const SideBarNav = styled.nav`
   display: flex;
@@ -9,16 +9,20 @@ export const SideBarNav = styled.nav`
   margin-right: auto;
   justify-content: center;
   width: 1184px;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding-top: 10px;
+  padding-bottom: 10px;
+
+  border-radius: 5px;
   ul {
     display: flex;
     list-style: none;
     padding: 0;
     margin: 0;
     justify-content: space-evenly;
-    background-color: rgba(0, 0, 0, 0.1);
+    width: 100%;
     height: 60px;
     align-items: center;
-    width: 1184px;
   }
 
   a {
@@ -26,22 +30,18 @@ export const SideBarNav = styled.nav`
     color: ${colors.textColor};
     font-size: 24px;
   }
-
-
-  
 `;
 
 export const SidebarLink = styled(NavLink)`
-margin: 0;
+  margin: 0;
   text-decoration: none;
   color: #333;
   font-size: 18px;
 
-  &:hover{
+  &:hover {
     color: ${colors.primaryHover};
     transition: color 0.3s ease-in-out;
   }
-
 
   &.active {
     font-weight: bold;
